a559696482f70800db1aa40e23ec458ec4a5cf8f
Nabeel Javed <i202420@nu.edu.pk>
2022-10-16 22:21:17 +0500

    added all functionalities

--- model/code.js
+++ model/code.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const schema = new mongoose.Schema({
     name:{
         type:String,
         required:true
     },
 })
-const userModel = mongoose.model("users", userSchema);
+const userModel = mongoose.model("users", schema);
 export default userModel;
--- server.js
+++ server.js
@@ -1,21 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import path from 'path';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+import userModel from './model/code.js';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const __dirname = path.dirname(__filename);
+const app = express();
+dotenv.config();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use(bodyParser.urlencoded({extended:true}))
 
-app.get('/', (req, res) =>{
-    res.send([1, 2, 3]);
-    }
-);
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Server is running.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+app.get("/",function(req,res){
+    
+  res.sendFile(__dirname+"/UI.html");
+  
+   
+  
+  })
 
 app.post('/', (req, res) =>{
-    res.send([1, 2, 3]);
+    var user = new userModel({
+        name: req.body.myname
+         }); user.save();
+        res.send("<h1>Your username has been stored in the Database. </h1>")
     }
 );
+
+app.listen(3000, () => {
+    connect();
+    console.log("Server is running on port 3000")
+  })
